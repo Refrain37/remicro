1491bebb1b16f8448b565a052c2d9f0dd653f59d
许泓杰 <965926581@qq.com>
2021-12-04 21:26:43 +0800

    test module link

--- packages/remicro.js/src/index.ts
+++ packages/remicro.js/src/index.ts
@@ -1,1 +1,9 @@
-const foo: string = 'foo'
+import utils from '@remicro.js/utils'
+
+const print = (a, b) => {
+  return utils.add(a, b)
+}
+
+export default {
+  print,
+}
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,7 @@
-const foo: string = 'foo'
+const add: (a: number, b: number) => number = (a, b) => {
+  return a + b
+}
+
+export default {
+  add,
+}
