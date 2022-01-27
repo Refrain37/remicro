034ba54fdb4214b27709bf87938b98b3a11aa277
许泓杰 <965926581@qq.com>
2022-01-27 21:51:03 +0800

    update btn

--- packages/button/src/teamplate.ts
+++ packages/button/src/teamplate.ts
@@ -1,16 +1,16 @@
 import { createTeamplate } from '@remicro.js/base-element';
 
 export default createTeamplate(function () {
-  const content = `${
+  const content = `<span>${
     this.loading == 'true'
-      ? '<rm-loading class="btn-loading" color="#fff"></rm-loading>'
+      ? '<rm-loading class="loading" color="#fff"></rm-loading>'
       : this.content
-  }`;
+  }</span>`;
   return `
-  <button class="rm-btn ${
+  <button  class="rm-btn ${
     this.type ? this.type + '-btn' : ''
-  } ${this.disabled && 'btn-disabled'}
-    ${this.loading == 'true' && 'btn-loading'}
+  } ${this.disabled ? 'btn-disabled' : ''}
+    ${this.loading == 'true' ? 'btn-loading' : ''}
     ${this.size}">
     ${content}
   </button>
