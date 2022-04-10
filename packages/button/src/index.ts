86e8809a90e14465dddc8489461abcd80757bfdd
许泓杰 <965926581@qq.com>
2022-04-10 21:11:32 +0800

    fix props setter

--- packages/button/src/index.ts
+++ packages/button/src/index.ts
@@ -40,20 +40,38 @@ export default class RmButton extends BaseElement {
   get content() {
     return this.getAttribute('content') || 'Button';
   }
+  set content(val) {
+    this.setAttribute('content', val);
+  }
   get type() {
     return this.getAttribute('type');
+  }
+  set type(val) {
+    this.setAttribute('type', val);
   }
   get size() {
     return this.getAttribute('size');
   }
+  set size(val) {
+    this.setAttribute('size', val);
+  }
   get disabled() {
     return this.getAttribute('disabled') !== null;
+  }
+  set disabled(val: any) {
+    this.setAttribute('disabled', val);
   }
   get href() {
     return this.getAttribute('href');
   }
+  set href(val: any) {
+    this.setAttribute('href', val);
+  }
   get loading() {
     return this.getAttribute('loading');
+  }
+  set loading(val: any) {
+    this.setAttribute('loading', val);
   }
 }
 
